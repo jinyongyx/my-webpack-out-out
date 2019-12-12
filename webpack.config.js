375578eb0c7f27a8b372656888d66301298a8ee8
ajin <1203867052@qq.com>
2019-12-12 11:25:55 +0800

    增加插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
 
+const HtmlWebpackPluin = require("html-webpack-plugin");
+
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
 module.exports = {
     entry:{
-        app:"./src/index.js",
+        index:"./src/index.js",
         print:"./src/print.js"
     },
 
     output:{
         filename:"[name].bundle.js",
         path:path.resolve(__dirname, "dist")
-    }
+    },
+
+    plugins:[
+        new HtmlWebpackPluin({
+            title:"插件生成的代码",
+            template:"src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
